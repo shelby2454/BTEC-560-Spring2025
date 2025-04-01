8b5b09ecbb095d5f62e2d3f818cb7ead352ef041
shelby2454 <143620008+shelby2454@users.noreply.github.com>
2025-04-01 10:15:41 -0400

    Add files via upload

--- Anova-Test.R
+++ Anova-Test.R
@@ -19,3 +19,21 @@ df1<- read.table(PATH, row.names= FALSE)
 
 #A simplier way
 df[,c(2:4)]
+
+#Load library ggplot
+library(ggplot2)
+
+#Plot your data using ggplot2
+ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
+
+#Apply the ANOVA function
+anova_one_way <- aov(time~poison, data = df)
+ 
+summary(anova_one_way)
+
+            Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
